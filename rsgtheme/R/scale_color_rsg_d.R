c9e6fd34dbdd50c5946d11a201d064e7c2f5e390
VikasMaturi <vikasmaturi@gmail.com>
2021-09-27 15:19:03 -0700

    documentation

--- rsgtheme/R/scale_color_rsg_d.R
+++ rsgtheme/R/scale_color_rsg_d.R
@@ -1,3 +1,10 @@
+#' Function to scale colors to the RSG theme
+#'
+#' This function uses the ggthemr package to create a function to scale colored plots to RSG colors.
+#' It will use the colors loaded in the rsgtheme function.
+#'
+
+
 scale_color_rsg_d <- function() {
 
   ggthemr::scale_colour_ggthemr_d()
