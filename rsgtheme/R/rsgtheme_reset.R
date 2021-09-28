facd4c55fbccd855bcda3d398cede8b707846a8d
VikasMaturi <vikasmaturi@gmail.com>
2021-09-28 08:55:30 -0700

    reset function and change color order

---
+++ rsgtheme/R/rsgtheme_reset.R
@@ -0,0 +1,11 @@
+#' MReset theme
+#'
+#' This function uses the ggthemr package to reset the document theme to the
+#' default colors from ggplot2
+#'
+
+rsgtheme_reset <- function() {
+
+  ggthemr::ggthemr_reset()
+
+}
