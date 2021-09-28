2af376b892ebf3e162f9d758d9579b200356d856
VikasMaturi <vikasmaturi@gmail.com>
2021-09-28 08:25:48 -0700

    get_colors function

---
+++ rsgtheme/R/get_colors.R
@@ -0,0 +1,11 @@
+#' Show default colors
+#'
+#' This function shows the default colors included in the rsgtheme package
+#'
+
+get_colors <- function() {
+
+  scales::show_col(c("#9D9999", "#E2D192", "#C55A11", "#3C485E", "#46566E", "#5C7292", "#F6BA92", "#F3EDD3", "#BCC6D5", "#595959", "#075861"))
+
+}
+
